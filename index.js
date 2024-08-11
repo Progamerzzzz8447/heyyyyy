f15c7db18a8db754055edd2922c18640400ab357
Progamerzzzz8447 <JH2010DH2013@HOTMAIL.COM>
2024-08-11 08:38:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["discord.gg/anzrblx","discord.gg/anzrblx"];
+const statusMessages = ["discord.gg/tui","Tui Roblox"];
 
 
 let currentIndex = 0;
